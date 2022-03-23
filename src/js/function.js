5eb5f17bdc2f59d7ad89dcd9fce0623a1a6cef81
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-03-23 17:13:53 +0100

    noscroll quand menu ouvert

--- src/js/function.js
+++ src/js/function.js
@@ -3,4 +3,6 @@ function Menu(){
     element.classList.toggle("header__toggle");
     var btn = document.getElementById("end");
     btn.classList.toggle("header__burger--open");
+    var noscroll = document.getElementById("html");
+    noscroll.classList.toggle("noscroll");
 }
